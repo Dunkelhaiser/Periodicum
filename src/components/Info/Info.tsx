443d2de38fd30e5f0cd5031cb78c832bcc6d51d5
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-05-01 14:40:19 +0300

    Added more details

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -11,18 +11,43 @@ const Info: React.FC = () => {
 
     return (
         <div className={S.info}>
-            {elementData && (
-                <div>
-                    <Element
-                        symbol={elementData.symbol}
-                        name={elementData.name}
-                        number={elementData.number}
-                        atomicMass={Math.round(elementData.atomicMass * 1e3) / 1e3}
-                        color={elementData.block as "s" | "p" | "d" | "f"}
-                    />
-                </div>
-            )}
-            <p>{elementData?.summary}</p>
+            <div className={S.summary}>
+                {elementData && (
+                    <div>
+                        <Element
+                            symbol={elementData.symbol}
+                            name={elementData.name}
+                            number={elementData.number}
+                            atomicMass={Math.round(elementData.atomicMass * 1e3) / 1e3}
+                            color={elementData.block as "s" | "p" | "d" | "f"}
+                        />
+                    </div>
+                )}
+                <p>{elementData?.summary}</p>
+            </div>
+            <div className={S.data}>
+                <p>
+                    <span>Discovered by:</span> {elementData?.discoveredBy}
+                </p>
+                {/* <p>
+                        <span>Named by:</span> {elementData?.namedBy || elementData?.discoveredBy}
+                    </p> */}
+                <p>
+                    <span>Atomic weight:</span> {elementData?.atomicMass}
+                </p>
+                <p>
+                    <span>Boiling point:</span> {elementData?.boil}
+                </p>
+                <p>
+                    <span>Melting point:</span> {elementData?.melt}
+                </p>
+                <p>
+                    <span>Density:</span> {elementData?.density}
+                </p>
+                <p>
+                    <span>Electron configuration:</span> {elementData?.electronConfiguration}
+                </p>
+            </div>
         </div>
     );
 };

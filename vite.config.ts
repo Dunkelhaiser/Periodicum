fa9cf0fc163d542d16dc628373ad8f7686edbb99
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-05-01 15:04:11 +0300

    Added PWA support'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        VitePWA({
+            manifest: {
+                name: "Periodicum",
+                short_name: "Periodicum",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#1a1c22",
+                lang: "en",
+                scope: "/",
+                icons: [
+                    {
+                        src: "/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+                theme_color: "#1a1c22",
+            },
+        }),
+    ],
 });

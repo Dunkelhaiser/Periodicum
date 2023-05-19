acf6432111cd0a77f85b75979db855b6a823e25e
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-05-19 11:26:43 +0300

    Added measurement units

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -8,14 +8,21 @@ import S from "./Info.module.scss";
 interface Props {
     label: string;
     value: string | number | undefined;
+    unit?: string;
+    capitalize?: boolean;
 }
 
-const Data: React.FC<Props> = ({ label, value }) => {
+const Data: React.FC<Props> = ({ label, value, unit, capitalize = true }) => {
     const nullValue = !value && value !== 0;
+    const formattedValue = nullValue ? "---" : formatValue(value);
+    const formattedUnit = unit && !nullValue && <span className={S.unit}>{unit === "%" || unit.includes("°") ? unit : ` ${unit}`}</span>;
     return (
         <div className={S.data}>
-            <p className={`${S.property} ${nullValue ? S.null : ""}`}>{label}</p>
-            <p className={`${S.value} ${nullValue ? S.null : ""}`}>{nullValue ? "---" : formatValue(value)}</p>
+            <p className={`${S.property} ${nullValue ? S.null : ""} ${S.capitalize}`}>{label}</p>
+            <p className={`${S.value} ${nullValue ? S.null : ""} ${capitalize ? S.capitalize : ""}`}>
+                {formattedValue}
+                {formattedUnit}
+            </p>
         </div>
     );
 };
@@ -34,19 +41,20 @@ const Info: React.FC = () => {
                 <Data label="Atomic number" value={elementData?.atomicNumber} />
                 <Data label="Period" value={elementData?.period} />
                 <Data label="Group" value={elementData?.group} />
-                <Data label="Block" value={elementData?.block} />
+                <Data label="Block" value={elementData?.block} capitalize={false} />
+                <Data label="State" value={elementData?.state} />
                 <Data label="Crystal structure" value={elementData?.crystalStructure} />
-                <Data label="Atomic mass" value={elementData?.atomicMass} />
-                <Data label="Density" value={elementData?.density} />
-                <Data label="Melting point" value={elementData?.meltingPoint} />
-                <Data label="Boiling point" value={elementData?.boilingPoint} />
+                <Data label="Atomic mass" value={elementData?.atomicMass} unit="g/mol" />
+                <Data label="Density" value={elementData?.density} unit="g/cm³" />
+                <Data label="Melting point" value={elementData?.meltingPoint} unit="°C" />
+                <Data label="Boiling point" value={elementData?.boilingPoint} unit="°C" />
                 <Data label="Electrons" value={elementData?.electronNumber} />
                 <Data label="Protons" value={elementData?.protonNumber} />
                 <Data label="Neutrons" value={elementData?.neutronNumber} />
                 <Data label="Valence electrons" value={elementData?.valenceElectrons} />
-                <Data label="Ionization energy" value={elementData?.ionizationEnergy} />
-                <Data label="Electron Affinity" value={elementData?.electronAffinity} />
-                <Data label="Atomic radius" value={elementData?.atomicRadius} />
+                <Data label="Ionization energy" value={elementData?.ionizationEnergy} unit="eV" />
+                <Data label="Electron Affinity" value={elementData?.electronAffinity} unit="kJ/mole" />
+                <Data label="Atomic radius" value={elementData?.atomicRadius} unit="pm" />
                 <Data label="Electron configuration" value={elementData?.electronConfiguration} />
                 <Data label="Electron configuration( semantic )" value={elementData?.electronConfigurationSemantic} />
             </div>
@@ -55,17 +63,17 @@ const Info: React.FC = () => {
                 <Data label="Electronegativity" value={elementData?.electronegativity} />
                 <Data label="Magnetic type" value={elementData?.magneticType} />
                 <Data label="Electrical type" value={elementData?.electricalType} />
-                <Data label="Electrical conductivity" value={elementData?.electricalConductivity} />
-                <Data label="Resistivity" value={elementData?.resistivity} />
-                <Data label="Superconducting point" value={elementData?.superconductingPoint} />
+                <Data label="Electrical conductivity" value={elementData?.electricalConductivity} unit="S/m" />
+                <Data label="Resistivity" value={elementData?.resistivity} unit="Ω·m" />
+                <Data label="Superconducting point" value={elementData?.superconductingPoint} unit="°C" />
             </div>
             <div className={S.section}>
                 <h3 className={S.title}>Abundance</h3>
-                <Data label="Universe" value={elementData?.abundanceUniverse} />
-                <Data label="Sun" value={elementData?.abundanceSun} />
-                <Data label="Earth crust" value={elementData?.abundanceEarth} />
-                <Data label="Oceans" value={elementData?.abundanceOceans} />
-                <Data label="Human body" value={elementData?.abundanceHumans} />
+                <Data label="Universe" value={elementData?.abundanceUniverse} unit="%" />
+                <Data label="Sun" value={elementData?.abundanceSun} unit="%" />
+                <Data label="Earth crust" value={elementData?.abundanceEarth} unit="%" />
+                <Data label="Oceans" value={elementData?.abundanceOceans} unit="%" />
+                <Data label="Human body" value={elementData?.abundanceHumans} unit="%" />
             </div>
             <div className={S.section}>
                 <h3 className={S.title}>Overview</h3>
@@ -73,6 +81,7 @@ const Info: React.FC = () => {
                 <Data label="Year of discovery" value={elementData?.yearDiscovered} />
                 <Data label="Discovered by" value={elementData?.discoveredBy} />
                 <Data label="CAS number" value={elementData?.casNumber} />
+                <Data label="Description" value={elementData?.description} capitalize={false} />
             </div>
         </ModalWindow>
     );

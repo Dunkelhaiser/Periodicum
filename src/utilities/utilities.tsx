47a26e65f9673e05e844b85fab6b537d004bef25
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-05-19 21:15:27 +0300

    Fixed foramtting function

--- src/utilities/utilities.tsx
+++ src/utilities/utilities.tsx
@@ -3,9 +3,8 @@ const handleChildElementClick = (e: React.FormEvent) => e.stopPropagation();
 const fixFloatingPoint = (val: number) => Number.parseFloat(val.toPrecision(15));
 
 const formatValue = (value: string | number | undefined) => {
-    const threshold = 1e-6;
     if (typeof value === "number" && value !== 0) {
-        if (Math.abs(value) < threshold || Math.abs(value) > -threshold) {
+        if (Math.abs(value) < 1e-6 || Math.abs(value) > 1e6) {
             const exponent = Math.floor(Math.log10(value));
             const coefficient = fixFloatingPoint(value / 10 ** exponent);
             const exponentFormatted = exponent.toString().replace(/^-/, "");
